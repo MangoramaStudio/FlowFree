95619b232e8b5a1620bcdda3c988371f74b0d18f
İrfan Yiğit BAYSAL <irfanbaysal1997@gmail.com>
2024-07-26 14:56:41 +0300

    Update header and top

--- Assets/TrailTracker.cs
+++ Assets/TrailTracker.cs
@@ -8,19 +8,18 @@ using UnityEngine;
 public class TrailTracker : MonoBehaviour
 {
     public List<Cell> positions = new();
-    public float speed = 0.25f;
+    [SerializeField] private float speed = 0.25f;
     
 
     [Button]
     public void Follow()
     {
-        transform.localPosition = positions[0].transform.position;
         
         Sequence sequence = DOTween.Sequence();
 
         foreach (Cell pos in positions)
         {
-            sequence.Append(transform.DOLocalMove(pos.transform.position, speed).SetEase(Ease.Linear));
+            sequence.Append(transform.DOMove(pos.transform.position, speed).SetEase(Ease.Linear));
         }
 
         sequence.Play();
