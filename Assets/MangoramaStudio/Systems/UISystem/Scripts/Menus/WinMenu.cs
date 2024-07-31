c882a4d0e54c875521e2d8f89ce82c64d18752c8
İrfan Yiğit BAYSAL <irfanbaysal1997@gmail.com>
2024-07-31 13:03:00 +0300

    Add claim page refactor

--- Assets/MangoramaStudio/Systems/UISystem/Scripts/Menus/WinMenu.cs
+++ Assets/MangoramaStudio/Systems/UISystem/Scripts/Menus/WinMenu.cs
@@ -8,7 +8,8 @@ namespace MangoramaStudio.Systems.UISystem.Scripts.Menus
     public class WinMenu : BaseMenu
     {
         [SerializeField] private TextMeshProUGUI areaTextTMP;
-
+        [SerializeField] private string preExplanation,postExplanation;
+        [SerializeField] private Color color;
         public override void Initialize()
         {
             base.Initialize();
@@ -18,7 +19,21 @@ namespace MangoramaStudio.Systems.UISystem.Scripts.Menus
         private void SetText()
         {
             var move = GameManager.Instance.UIManager.GameplayMenu().GetMoveCount();
-            areaTextTMP.SetText($"You cleared in {move} moves");
+          //  areaTextTMP.SetText($"You cleared in {move} moves");
+          Set();
+        }
+        
+        public string ColorString(string text, Color color)
+        {
+            return "<color=#" + ColorUtility.ToHtmlStringRGBA(color) + ">" + text + "</color>";
+        }
+
+        public void Set()
+        {
+            var move = GameManager.Instance.UIManager.GameplayMenu().GetMoveCount();
+            var coloredExplanation = ColorString(move.ToString(), color);
+            var text = $"{preExplanation} {coloredExplanation} {postExplanation}";
+            areaTextTMP.SetText(text);
         }
     }
 }
